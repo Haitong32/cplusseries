c4ea7ab86558887f8f9b42bef702047c035e42c6
Haitong32 <zhenh522@newschool.edu>
2018-06-04 22:44:38 +0800

    update final(2)

--- script-press.js
+++ script-press.js
@@ -26,42 +26,4 @@ $(document).ready(function(){
     effect: "fadeIn"
 
 });
-  $("#tag-18AW").click(function(){
-    $(".img").lazyload({effect: "fadeIn"
-      });
-    $("#tag-18AW").css("border-bottom","1px solid red");
-    $("#tag-18SS").css("border-bottom","none");
-    $("#tag-17AW").css("border-bottom","none");
-    $("#tag-17SS").css("border-bottom","none");
-    $('html, body').animate({
-          scrollTop: $("#18AW").offset().top
-      });
-  });
-  $("#tag-18SS").click(function(){
-    $("#tag-18SS").css("border-bottom","1px solid red");
-    $("#tag-18AW").css("border-bottom","none");
-    $("#tag-17AW").css("border-bottom","none");
-    $("#tag-17SS").css("border-bottom","none");
-    $('html, body').animate({
-          scrollTop: $("#18SS").offset().top
-      });
-  });
-  $("#tag-17AW").click(function(){
-    $("#tag-17AW").css("border-bottom","1px solid red");
-    $("#tag-18AW").css("border-bottom","none");
-    $("#tag-18SS").css("border-bottom","none");
-    $("#tag-17SS").css("border-bottom","none");
-    $('html, body').animate({
-          scrollTop: $("#17AW").offset().top
-      });
-  });
-  $("#tag-17SS").click(function(){
-    $("#tag-17SS").css("border-bottom","1px solid red");
-    $("#tag-18AW").css("border-bottom","none");
-    $("#tag-17AW").css("border-bottom","none");
-    $("#tag-18SS").css("border-bottom","none");
-    $('html, body').animate({
-          scrollTop: $("#17SS").offset().top
-      });
-  });
 });
